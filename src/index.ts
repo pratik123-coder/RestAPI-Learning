0ceddb20eae2eaf8313ffd5ecec07ed7105bafd3
pratik-win-PC <pratiktravelogue@gmail.com>
2023-11-07 00:48:22 +0530

    Users.ts done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -18,5 +19,11 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-  console.log(`Server running on http://localhost:8080/`)
+  console.log('Server running on http://localhost:8080/')
 });
+
+const MONGO_URL = 'mongodb+srv://pratik:Pkmty!23@cluster0.t0d9wbj.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
